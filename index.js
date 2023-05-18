a944eb82a5f36a522086b007083041f6a411b092
NouraNou <oneway.33@hotmail.com>
2023-05-18 17:35:43 +0300

    owful

--- index.js
+++ index.js
@@ -2,3 +2,66 @@ const employeesJSON = require('./employees.json');
 const managersJSON = require('./managers.json');
 
 // Code Here
+class Employee {
+"name" 
+"position"
+"yearJoied"
+"salary"
+
+    constructor(name, position ,yearJoied, salary)
+    {
+        this.name = name;
+        this.position = position;
+        this.yaerJpoined = yearJpined;
+        this.salary = salary;
+    } 
+
+    Employee.function (idbadge){
+
+      Employee.emloyees.filter((emp1) => {
+        
+        return ( "<" + emp1.position + ">" + ":  " +"<" + emp1.name + ">")});
+
+    
+
+    }
+}
+
+class Maneger extend Employee {
+
+    bonusPercentage;
+
+    constructor(name, position, yearJoined , salary, bonusPercentage){
+        super(name , position, yaerJoined, salary);
+        this.bonusPercentage = bonusPercentage;
+         
+
+    }
+    
+
+       function salaryIncreas(Emloyee){
+
+        
+
+       }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
